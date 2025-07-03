a0fa209e68c2471d54bc470e419c2c518f9a95f1
rishabh <rishabh.divyanshu1234@gmail.com>
2025-07-03 10:33:41 +0530

    Problem3

---
+++ HackWithInfy2025/DIStringMatch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class DIStringMatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = s.length();
+
+        int low = 0, high = n;
+        int[] result = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'I') {
+                result[i] = low++;
+            } else {
+                result[i] = high--;
+            }
+        }
+        result[n] = low; 
+        System.out.println(Arrays.toString(result));
+    }
+}
