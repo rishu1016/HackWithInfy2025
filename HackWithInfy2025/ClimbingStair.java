77838195f72305ec423f7ea839847edfbc1f6176
rishabh <rishabh.divyanshu1234@gmail.com>
2025-07-07 12:32:45 +0530

    Day 3

---
+++ HackWithInfy2025/ClimbingStair.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class ClimbingStair {
+    public static int countWays(int n){
+
+        if(n==1) return 1;
+        if(n==2) return  2;
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+
+        }
+        return dp[n];
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((countWays(n)));
+    }    
+}
